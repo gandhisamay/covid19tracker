b8316710419f35572d9872bac895106989c41752
gandhisamay <gandhisamay200@gmail.com>
2021-07-25 17:07:42 +0530

    data updated using API

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-const search = document.querySelector('form');
-const input = document.querySelector('.form-control')
+const form = document.querySelector('.navmain form');
+const searchBar = document.querySelector('.navmain input');
 
-let getData = async ()=>{
-    let countryWiseCases = await axios.get("https://corona-api.com/countries/AF");
-    console.log(countryWiseCases);
-}
+const overallDeaths = document.querySelector('#overall-deaths');
+const overallConfirmed = document.querySelector('#overall-confirmed');
+const overallRecovered = document.querySelector('#overall-recovered');
+const overallCritical = document.querySelector('#overall-critical');
+const todayDeaths = document.querySelector('#today-deaths');
+const todayConfirmed = document.querySelector('#today-confirmed');
+const countryName = document.querySelector('.country');
 
-getData();
+let searchCountry;
 
-search.addEventListener('submit', (e)=>{
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(input.value);
+    searchCountry = searchBar.value;
+    getData(searchCountry);
 })
 
+let getData = async (searchCountry) => {
+    try {
+        let countryWiseCases = await axios.get("https://corona-api.com/countries");
+        for (data of countryWiseCases.data.data) {
+            if (searchCountry.toLowerCase() == data.name.toLowerCase()) {
+                console.log(data.name);
 
+                countryName.innerText = data.name;
+                overallDeaths.innerText = data.latest_data.deaths;
+                overallConfirmed.innerText = data.latest_data.confirmed;
+                overallRecovered.innerText = data.latest_data.recovered;
+                overallCritical.innerText = data.latest_data.critical;
 
+                todayConfirmed.innerText = data.today.confirmed;
+                todayDeaths.innerText = data.today.deaths;
+                break;
+            }
+
+        }
+    } catch{
+        console.log("OOPs looks like some error has occured");
+    }
+}
