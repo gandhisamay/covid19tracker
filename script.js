4824902515f7ec2e1b3062513f6f06ee4dfa0aba
gandhisamay <gandhisamay200@gmail.com>
2021-07-26 12:26:19 +0530

    Chart included

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const form = document.querySelector('.navmain form');
 const searchBar = document.querySelector('.navmain input');
-
 const overallDeaths = document.querySelector('#overall-deaths');
 const overallConfirmed = document.querySelector('#overall-confirmed');
 const overallRecovered = document.querySelector('#overall-recovered');
@@ -10,6 +9,42 @@ const yesterdayConfirmed = document.querySelector('#yesterday-confirmed');
 const yesterdayRecovered = document.querySelector('#yesterday-recovered');
 const countryName = document.querySelector('.country');
 const lastUpdated = document.querySelector('.last-updated');
+const ctx = document.querySelector('#confirmed-chart');
+
+const chart = new Chart(ctx, {
+    type: 'line',
+    data:{
+        labels:['Jan', 'Feb','Mar','May', 'June', 'July','Aug', 'Sept', 'Oct', 'Nov', 'Dec'],
+        datasets : [{
+            labels: 'Covid Cases',
+            data:[8,9,10,1,2,3,4,5,6,7,8,9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 3,
+        }],
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
 
 let searchCountry = 'India';
 let countryCode;
