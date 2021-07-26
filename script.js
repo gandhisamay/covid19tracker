214cac164a42f0d2aec2d049d7cba762be37de10
gandhisamay <gandhisamay200@gmail.com>
2021-07-26 11:18:54 +0530

    Chart and page 2 remaining

--- script.js
+++ script.js
@@ -5,38 +5,54 @@ const overallDeaths = document.querySelector('#overall-deaths');
 const overallConfirmed = document.querySelector('#overall-confirmed');
 const overallRecovered = document.querySelector('#overall-recovered');
 const overallCritical = document.querySelector('#overall-critical');
-const todayDeaths = document.querySelector('#today-deaths');
-const todayConfirmed = document.querySelector('#today-confirmed');
+const yesterdayDeaths = document.querySelector('#yesterday-deaths');
+const yesterdayConfirmed = document.querySelector('#yesterday-confirmed');
+const yesterdayRecovered = document.querySelector('#yesterday-recovered');
 const countryName = document.querySelector('.country');
+const lastUpdated = document.querySelector('.last-updated');
 
-let searchCountry;
+let searchCountry = 'India';
+let countryCode;
+
+let updateOverallData = (searchCountry, countryWiseCases) => {
+    for (data of countryWiseCases.data.data) {
+        if (searchCountry.toLowerCase() == data.name.toLowerCase()) {
+            console.log(data);
+
+            countryName.innerText = data.name;
+            overallDeaths.innerText = data.latest_data.deaths;
+            overallConfirmed.innerText = data.latest_data.confirmed;
+            overallRecovered.innerText = data.latest_data.recovered;
+            overallCritical.innerText = data.latest_data.critical;
+
+            let {timeline} = data;
+            yesterdayConfirmed.innerText = timeline[0].new_confirmed;
+            yesterdayDeaths.innerText = timeline[0].new_deaths;
+            yesterdayRecovered.innerText = timeline[0].new_recovered;
+            lastUpdated.innerText = `${timeline[0].updated_at.slice(0,10)} ${timeline[0].updated_at.slice(11,19)}`;
+            break;
+        }
+
+    }
+}
+
+let basicDataLink = "https://corona-api.com/countries";
+
+let getData = async (searchCountry,link) => {
+    try {
+        let countryWiseCases = await axios.get(link, {params:{include: 'timeline'}});
+
+        updateOverallData(searchCountry, countryWiseCases);
+
+    } catch(err) {
+        console.log("OOPs looks like some error has occured",err);
+    }
+}
+
+getData(searchCountry, basicDataLink);
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     searchCountry = searchBar.value;
-    getData(searchCountry);
+    getData(searchCountry, basicDataLink);
 })
-
-let getData = async (searchCountry) => {
-    try {
-        let countryWiseCases = await axios.get("https://corona-api.com/countries");
-        for (data of countryWiseCases.data.data) {
-            if (searchCountry.toLowerCase() == data.name.toLowerCase()) {
-                console.log(data.name);
-
-                countryName.innerText = data.name;
-                overallDeaths.innerText = data.latest_data.deaths;
-                overallConfirmed.innerText = data.latest_data.confirmed;
-                overallRecovered.innerText = data.latest_data.recovered;
-                overallCritical.innerText = data.latest_data.critical;
-
-                todayConfirmed.innerText = data.today.confirmed;
-                todayDeaths.innerText = data.today.deaths;
-                break;
-            }
-
-        }
-    } catch{
-        console.log("OOPs looks like some error has occured");
-    }
-}
